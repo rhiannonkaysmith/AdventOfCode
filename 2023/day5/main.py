75ce754742028a7d3556d576781a38d9ebf26a53
Rhiannon Smith <rhiannonsmith@rhiannons-mbp.home>
2024-03-17 12:11:20 +0000

    tidy up solution 1

--- 2023/day5/main.py
+++ 2023/day5/main.py
@@ -1,45 +1,24 @@
 import re
 
 with open('input.txt', 'r') as input_file:
-    mappings = input_file.read().split('\n\n')
+    seeds, *mappings = input_file.read().split('\n\n')
+    seeds = list(map(int, seeds.split()[1:]))
+
+
+def new_position(s, mapping):
+    for m in mapping:
+        for conversions in re.findall(r'(\d+) (\d+) (\d+)', m):
+            destination, start, length = map(int, conversions)
+            if s in range(start, start + length):
+                s += destination - start
+                break
+    return s
 
 
 def solution():
-    seeds = re.findall(r'\d+', mappings[0])
-
-    locations = []
-    for s in map(int, seeds):
-        # gets the seeds from seeds: 79 14 55 13 -> 79, 14, 55, 13
-        for m in mappings[1:]:
-            for conversions in re.findall(r'(\d+) (\d+) (\d+)', m):
-                destination, start, length = map(int, conversions)
-                if s in range(start, start + length):
-                    s += destination - start
-                    break
-        locations.append(s)
-    print(f'lowest location:{min(locations)}')
-
-
-def solution_2():
-    seeds = re.findall(r'(\d+) (\d+)', mappings[0])
-
-    new_seeds = []
-    for seed, length in seeds:
-        for s in list(range(int(seed), int(seed) + int(length))):
-            new_seeds.append(s)
-
-    # locations = []
-    # for s in new_seeds:
-    #     for m in mappings[1:]:
-    #         for conversions in re.findall(r'(\d+) (\d+) (\d+)', m):
-    #             destination, start, length = map(int, conversions)
-    #             if s in range(start, start + length):
-    #                 s += destination - start
-    #                 break
-    #     locations.append(s)
-    # print(f'solution 2 lowest location:{min(locations)}')
+    end_positions = [new_position(s, mappings) for s in seeds]
+    print(f'solution 1: {min(end_positions)}')
 
 
 if __name__ == '__main__':
     solution()
-    # solution_2()
